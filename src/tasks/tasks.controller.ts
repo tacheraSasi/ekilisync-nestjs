97154ecd2b42d66581b0c77eb62b7b1859ace9f9
Tachera Sasi <tacherasasi@gmail.com>
2025-03-10 11:01:13 +0300

    add methods to TasksController for creating, completing, and retrieving tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Patch, Get, Body, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post()
+  createTask(@Body() body: { title: string; ownerId: string }) {
+    return this.tasksService.createTask(body.title, body.ownerId);
+  }
+
+  @Patch(':id/complete')
+  markCompleted(@Param('id') taskId: string) {
+    return this.tasksService.markCompleted(taskId);
+  }
+
+  @Get(':userId')
+  getTasks(@Param('userId') userId: string) {
+    return this.tasksService.getTasks(userId);
+  }
+}
