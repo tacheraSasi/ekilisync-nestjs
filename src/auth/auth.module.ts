e5b9f719edafbdab5479e86db1db6f1c377d3786
Tachera Sasi <tacherasasi@gmail.com>
2025-03-10 12:06:00 +0300

    add JWT authentication and Prisma service lifecycle management

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
