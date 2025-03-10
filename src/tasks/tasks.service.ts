001170df5a79ea918e7908399f0021b9195932bd
Tachera Sasi <tacherasasi@gmail.com>
2025-03-10 11:00:45 +0300

    implement TasksService with methods to create, mark as completed, and retrieve tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private prisma: PrismaService) {}
+
+  async createTask(title: string, ownerId: string) {
+    return this.prisma.task.create({
+      data: { title, ownerId },
+    });
+  }
+
+  async markCompleted(taskId: string) {
+    return this.prisma.task.update({
+      where: { id: taskId },
+      data: { completed: true },
+    });
+  }
+
+  async getTasks(userId: string) {
+    return this.prisma.task.findMany({
+      where: { ownerId: userId },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+}
