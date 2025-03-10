bb5f7d75fa094ce6f4ee0daccda73bdb1306c784
Tachera Sasi <tacherasasi@gmail.com>
2025-03-10 11:06:29 +0300

    add JwtModule to AppModule for JWT authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
 import { TasksModule } from './tasks/tasks.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot(), TasksModule],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot(),
+    TasksModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
