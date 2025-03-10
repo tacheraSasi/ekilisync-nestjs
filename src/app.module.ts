33d06a22128060a54a2681a2dbcaeee1c042f5d0
Tachera Sasi <tacherasasi@gmail.com>
2025-03-10 10:58:08 +0300

    add Tasks module with controller and service, and include it in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma/prisma.service';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot()],
+  imports: [AuthModule, ConfigModule.forRoot(), TasksModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
