3257474267ac00d8bf55723ef413d2821ac03f0a
Tachera Sasi <tacherasasi@gmail.com>
2025-03-10 10:32:43 +0300

    implement AuthService with signUp and signIn methods, and inject PrismaService and JwtService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {
+    console.log('AuthService instantiated');
+  }
+
+  async signUp() {
+    console.log('Sign up');
+  }
+
+  async signIn() {
+    console.log('Sign in');
+  }
+}
