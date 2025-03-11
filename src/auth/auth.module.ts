cecb50dbcdd4cb79261e25ded4132f73718a57be
Tachera Sasi <tacherasasi@gmail.com>
2025-03-11 10:33:21 +0300

    add UsersService to AuthModule imports for user validation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { PrismaModule } from '../prisma/prisma.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthGuard } from './auth.guard';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
   imports: [
     PrismaModule,
+    UsersService,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '20h' },
