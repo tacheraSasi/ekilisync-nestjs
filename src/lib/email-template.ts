23874e44faf364d1706e33de1022917b1155603d
Tachera Sasi <tacherasasi@gmail.com>
2025-03-11 09:36:01 +0300

    refactor email template to use an interface for options and update logo URL

--- src/lib/email-template.ts
+++ src/lib/email-template.ts
@@ -1,12 +1,22 @@
-export const ekiliSyncEmailTemplate = (
+interface EmailTemplateOptions {
+  type: string;
+  title: string;
+  message: string;
+  buttonLink?: string;
+  buttonText?: string;
+  companyName?: string;
+  logoUrl?: string;
+}
+
+export const ekiliSyncEmailTemplate = ({
   type,
   title,
   message,
   buttonLink = '#',
   buttonText = 'Take Action',
   companyName = 'EkiliSync',
-  logoUrl = 'https://relay.ekilie.com/img/favicon.png',
-) => {
+  logoUrl = 'https://example.com/ekilisync-logo.png',
+}: EmailTemplateOptions) => {
   return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office" lang="en">
   <head>
