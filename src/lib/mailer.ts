22d016489d026c90b9a9980f6545ff0bcc147ad4
Tachera Sasi <tacherasasi@gmail.com>
2025-03-11 09:01:37 +0300

    add sendEmail method to Mailer class for sending emails

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,9 +1,13 @@
 import EkiliRelay from 'ekilirelay';
 
 export class Mailer extends EkiliRelay {
-//   private readonly apiKey: string = process.env.RELAY_API_KEY;
+  //   private readonly apiKey: string = process.env.RELAY_API_KEY;
 
   constructor() {
     super(process.env.RELAY_API_KEY);
+  }
+
+  async sendEmail(to: string, subject: string, message: string): Promise<any> {
+    return this.sendEmail(to, subject, message);
   }
 }
